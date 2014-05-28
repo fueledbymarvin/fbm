0bb404bd4ce992950752ca8bf36b568cecb540da
Marvin Qian <fueledbymarvin@gmail.com>
2014-05-28 13:01:46 -0400

    Setup capistrano 3

---
+++ config/deploy.rb
@@ -0,0 +1,81 @@
+set :application, 'fbm'
+set :user, 'deploy'
+
+# setup repo details
+set :scm, :git
+set :repo_url, 'git://github.com/fueledbymarvin/fbm.git'
+
+# setup rvm.
+set :rvm1_ruby_version, '2.1.1'
+
+# how many old releases do we want to keep, not much
+set :keep_releases, 5
+
+# files we want symlinking to specific entries in shared
+set :linked_files, %w{config/database.yml}
+
+# dirs we want symlinking to shared
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
+
+# which config files should be copied by deploy:setup_config
+# see documentation in lib/capistrano/tasks/setup_config.cap
+# for details of operations
+set(:config_files, %w(
+  nginx.conf
+  database.yml
+  puma_init.sh
+  puma.rb
+))
+
+# which config files should be made executable after copying
+# by deploy:setup_config
+set(:executable_config_files, %w(
+  puma_init.sh
+))
+
+
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc. 
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/#{fetch(:application)}"
+  },
+  {
+    source: "puma_init.sh",
+    link: "/etc/init.d/puma_#{fetch(:application)}"
+  }
+])
+
+# this:
+# http://www.capistranorb.com/documentation/getting-started/flow/
+# is worth reading for a quick overview of what tasks are called
+# and when for `cap stage deploy`
+
+namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, "deploy:check_revision"
+  # only allow a deploy with passing tests to deployed
+  before :deploy, "deploy:run_tests"
+  # compile assets locally then rsync
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
+
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
+end
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,23 @@
+set :stage, :production
+set :branch, "master"
+
+# This is used in the Nginx VirtualHost to specify which domains
+# the app should appear on. If you don't yet have DNS setup, you'll
+# need to create entries in your local Hosts file for testing.
+set :server_name, "www.fueledbymarv.in fueledbymarv.in"
+
+server '107.170.182.106', user: 'deploy', roles: %w{web app db}, primary: true
+
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+# dont try and infer something as important as environment from
+# stage name.
+set :rails_env, :production
+
+# number of unicorn workers, this will be reflected in
+# the unicorn.rb and the monit configs
+set :unicorn_worker_count, 5
+
+# whether we're using ssl or not, used for building nginx
+# config file
+set :enable_ssl, false
