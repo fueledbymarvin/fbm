8b904c9ac791a82706940e5e4c268a1cd63cd9c0
Marvin Qian <fueledbymarvin@gmail.com>
2014-05-28 16:02:57 -0400

    Debug environment variable issues

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,3 +1,7 @@
+p ENV['SECRET_KEY_BASE']
+p ENV['FBM_ROOT_URL']
+p "HELLO??"
+
 Slim::Engine.set_default_options :attr_delims => {'(' => ')', '[' => ']'}
 
 Rails.application.assets.register_engine('.slim', Slim::Template)
