d973a19da51825d99e79d2fe437cc53bdf2fe460
Marvin Qian <fueledbymarvin@gmail.com>
2014-05-28 14:08:29 -0400

    Fix capistrano issues

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ set :rails_env, :production
 
 # number of unicorn workers, this will be reflected in
 # the unicorn.rb and the monit configs
-set :unicorn_worker_count, 5
+set :puma_worker_count, 5
 
 # whether we're using ssl or not, used for building nginx
 # config file
