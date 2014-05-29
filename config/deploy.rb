0a8f4207e32057618d297c63051e525f4aebd096
Marvin Qian <fueledbymarvin@gmail.com>
2014-05-28 23:35:20 -0400

    Reorder capistrano tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,22 +60,24 @@ set(:symlinks, [
 
 namespace :deploy do
   # make sure we're deploying what we think we're deploying
-  before :deploy, "deploy:check_revision"
+  before :deploy, 'deploy:check_revision'
+
   # only allow a deploy with passing tests to deployed
-  before :deploy, "deploy:run_tests"
-  # compile assets locally then rsync
-  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
-  after :finishing, 'deploy:cleanup'
+  # before :deploy, 'deploy:run_tests'
 
   # remove the default nginx configuration as it will tend
   # to conflict with our configs.
+  after 'deploy:check_revision', 'deploy:setup_config'
   before 'deploy:setup_config', 'nginx:remove_default_vhost'
-
   # reload nginx to it will pick up any modified vhosts from
   # setup_config
   after 'deploy:setup_config', 'nginx:reload'
 
+  # compile assets locally then rsync
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
+
   # As of Capistrano 3.1, the `deploy:restart` task is not called
   # automatically.
-  after 'deploy:finished', 'deploy:restart'
+  after 'deploy:publishing', 'deploy:restart'
 end
